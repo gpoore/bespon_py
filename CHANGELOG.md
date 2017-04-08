ed6521a6ae403b7473dc242da4659be05db0e63c
Geoffrey Poore <gpoore@gmail.com>
2017-04-07 19:11:43 -0500

    v0.1.0

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# `bespon` Change Log
+
+## v0.1.0 (2017-04-07)
+
+* First release with mostly complete basic feature set.
--- bespon/version.py
+++ bespon/version.py
@@ -4,4 +4,4 @@ from __future__ import (division, print_function, absolute_import,
                         unicode_literals)
 
 from .fmtversion import get_version_plus_info
-__version__, __version_info__ = get_version_plus_info(0, 1, 0, 'dev', 2)
+__version__, __version_info__ = get_version_plus_info(0, 1, 0, 'final', 0)
