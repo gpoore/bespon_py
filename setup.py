5843ff88ff4d0d2b66c2f46f04d3b06d0f3b25aa
Geoffrey Poore <gpoore@gmail.com>
2020-11-02 06:45:10 -0600

    update setup.py for recent Python versions

--- setup.py
+++ setup.py
@@ -75,6 +75,9 @@ setup(name = 'BespON',
           'Programming Language :: Python :: 3.4',
           'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
           'Topic :: Utilities',
       ]
 )
